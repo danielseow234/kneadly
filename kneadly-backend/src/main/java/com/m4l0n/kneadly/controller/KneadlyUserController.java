b76d91e53c2201dd3b5deef949a0b631c7791ec7
Nigel <tp062253@mail.apu.edu.my>
2023-07-31 18:03:09 +0800

    add request mapping for user api

--- kneadly-backend/src/main/java/com/m4l0n/kneadly/controller/KneadlyUserController.java
+++ kneadly-backend/src/main/java/com/m4l0n/kneadly/controller/KneadlyUserController.java
@@ -9,11 +9,14 @@ import com.m4l0n.kneadly.response.ResponseAPI;
 import com.m4l0n.kneadly.response.StatusCode;
 import com.m4l0n.kneadly.service.KneadlyUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/user")
+@RestController
+@RequestMapping(value = "/user", produces = MediaType.APPLICATION_JSON_VALUE)
 public class KneadlyUserController {
 
     private final KneadlyUserService kneadlyUserService;
