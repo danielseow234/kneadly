b7c35459b564bdae7cc370d6976f17eb2e50f860
danielseow234 <danielseow234@gmail.com>
2023-08-09 09:54:23 +0800

    Update axios.js

--- kneadly-frontend/src/api/axios.js
+++ kneadly-frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://kneadly-env.eba-eh3npqmp.us-east-1.elasticbeanstalk.com' // change the baseURL
+    baseURL: 'https://6rf284jfm6.execute-api.us-east-1.amazonaws.com/prod' // change the baseURL
     // baseURL: 'http://localhost:5000' // change the baseURL
 });
